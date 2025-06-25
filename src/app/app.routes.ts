bac1f7f19476996316e8d7007d52eb998e322647
Edu14RS <eduardo14.05rivas@gmail.com>
2025-06-24 20:57:30 -0500

    feat: Agregado de interfaces gráficas y funcionalidades para login y registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/public_/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        loadChildren: () => import('./pages/auth/auth.routes').then(a=>a.authRoutes)
+    },
+    {
+        path: '',
+        component: LandingComponent
+    }
+];
+
