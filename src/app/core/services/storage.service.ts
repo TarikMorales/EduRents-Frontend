566529420c3cfebf320181d3035a34c6176b96ba
Edu14RS <eduardo14.05rivas@gmail.com>
2025-06-26 13:38:23 -0500

    feat: Agregado de funcionalidades e interfaces gráficas para el proceso de cambio de contrasena

--- src/app/core/services/storage.service.ts
+++ src/app/core/services/storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AuthResponse } from '../../shared/model/login/auth-response.model';
+import { RecoverProcessResponse } from '../../shared/model/forgot-password/recover-process-response.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,12 +8,22 @@ import { AuthResponse } from '../../shared/model/login/auth-response.model';
 export class StorageService {
 
   private authKey = 'edurents_auth';
+  private recoverKey = 'edurents_recover';
 
   constructor() {}
 
   setAuthData(data: AuthResponse): void {
     console.log(data);
     localStorage.setItem(this.authKey, JSON.stringify(data));
+  }
+
+  setRecoverData(data: RecoverProcessResponse): void {
+    localStorage.setItem(this.recoverKey, JSON.stringify(data));
+  }
+
+  getRecoverData(): RecoverProcessResponse | null {
+    const data = localStorage.getItem(this.recoverKey);
+    return data ? JSON.parse(data) as RecoverProcessResponse : null;
   }
 
   getAuthData(): AuthResponse | null {
@@ -22,5 +33,9 @@ export class StorageService {
 
   clearAuthData(): void {
     localStorage.removeItem(this.authKey);
+  }
+
+  clearRecoverData(): void {
+    localStorage.removeItem(this.recoverKey);
   }
 }
