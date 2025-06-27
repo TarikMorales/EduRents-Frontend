c8e4e3a0d9db70a6f2058ce4551ac466143fe68b
leo-dev11 <U202213866@upc.edu.pe>
2025-06-26 23:15:02 -0500

    feature: Se agregó el diseño para la página de buscar productos, así como la ventana de filtros y las product-card de los productos.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {ProductListComponent} from "./pages/public_/product-list/product-list.component";
+import {ProductCardComponent} from "./pages/public_/product-list/product-card/product-card.component"
+import {FilterProductListComponent} from "./pages/public_/product-list/filter-product-list/filter-product-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'product-list',
+    component: ProductListComponent,
+  },
+  {
+    path: 'product-card',
+    component: ProductCardComponent,
+  },
+  {
+    path: 'filter-product-list',
+    component: FilterProductListComponent,
+  }
+];
--- src/app/pages/public_/product-list/product-list.component.ts
+++ src/app/pages/public_/product-list/product-list.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {ProductCardComponent} from "./product-card/product-card.component";
+import {FilterProductListComponent} from "./filter-product-list/filter-product-list.component";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,ProductCardComponent, FilterProductListComponent
+  ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  mostrarFiltros: boolean = false;
 
+  alternarFiltros() {
+    this.mostrarFiltros = !this.mostrarFiltros;
+  }
+
+  filtrarProductos(filtros: any) {
+    console.log('Filtros aplicados:', filtros);
+    // Aquí luego filtrarás la lista de productos
+  }
 }
