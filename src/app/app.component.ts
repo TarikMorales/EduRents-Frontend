e9d91a95c9681598ecafd94f579bcfc9b091be45
MapleSyrup63 <rodrigo,ramirez.2005@gmail.com>
2025-06-27 04:36:27 -0500

    Feature frontend de HU01 concluido (sin inclusion de API)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MYproductsComponent } from "./pages/public_/myproducts/myproducts.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MYproductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
